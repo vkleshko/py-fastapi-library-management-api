4456cd9ebe513bad8cbec21118bd73980a3e55de
Vlad Kleshko <vkoesko@gmail.com>
2024-06-26 23:04:42 +0300

    Created first migrations

---
+++ alembic/versions/05a719843ead_migrations_0001.py
@@ -0,0 +1,26 @@
+"""Migrations 0001
+
+Revision ID: 05a719843ead
+Revises: 
+Create Date: 2024-06-26 23:00:48.566044
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '05a719843ead'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
