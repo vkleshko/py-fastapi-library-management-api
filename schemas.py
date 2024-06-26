f677c006f94149eb945aa5361fba1b5396c23b1b
Vlad Kleshko <vkoesko@gmail.com>
2024-06-26 23:37:30 +0300

    Created serialization for Book model

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from datetime import date
 from pydantic import BaseModel
 
 from db.models import Book
@@ -11,12 +11,30 @@ class AuthorBase(BaseModel):
     books: List[Book] | None
 
 
-class AuthorCreate(BaseModel):
+class AuthorCreate(AuthorBase):
     pass
 
 
 class Author(AuthorBase):
     id: int
+
+    class Config:
+        orm_mode = True
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication: date
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class Book(BookBase):
+    id: int
+    author: Author
 
     class Config:
         orm_mode = True
